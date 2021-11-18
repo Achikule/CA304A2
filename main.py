ecd5fb54e054149a1f2ebd749e704f297e2b08cf
Achikule <achikule279@gmail.com>
2021-11-18 23:02:34 +0000

    Final commented and documented version

--- main.py
+++ main.py
@@ -3,9 +3,49 @@
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
+description = """
+This script requires the following modules to run: Pydantic, typing and FastAPI
+"""
 
 
-app = FastAPI(title="Alen Joy CA304 Assessment 2")
+tags_metadata = [
+    {
+        "name": "addrouter",
+        "description": """This endpoint instantiates a RouterGraph object and adds a router from input as one of 
+        the vertices in an undirected graph. Each vertex is given an ID based on the order they were added. 
+        An (n x n) adjacency matrix is initialized where n is the number of the number of vertices. 
+        If successfully created, this message will be returned: {"status": "success"}. 
+        If the router already exists, this message will be returned: {"status": "Error, node already exists"}""",
+    },
+    {
+        "name": "connect",
+        "description": """This endpoint takes in two routers to connect and the weight/distance of that connection.
+        This weight is updated on the adjacency matrix. If the input is a new connection definition, this message is
+        returned: {"status": "success"}. If the connection between the 2 routers was defined before and the function is 
+        called again, the weight will be updated and this message is returned: {"status": "updated"}. If one or more
+        of the routers don't exist, this message is returned: {"status": "Error, router does not exist"}
+        """
+
+
+    },
+    {
+        "name": "removerouter",
+        "description": """This endpoint removes the router given to it. First, the router is removed from vertices and
+        the IDs of all vertices are updated to reflect this. Next, the row and column of this router is removed from the
+        matrix, removing all pre-existing connections to other routers. If this process is successful, this message
+        is returned {"status": "success"}""",
+    },
+    {
+        "name": "removeconnection",
+        "description": """This endpoint removes a connection between any two routers that exist in vertices. This is 
+        achieved by setting the values in the adjacency matrix where they connect back to -1. If successful, 
+        {"status": "success"} is returned.""",
+    }
+]
+
+app = FastAPI(title="Alen Joy CA304 Assessment 2",
+              description=description,
+              openapi_tags=tags_metadata)
 
 
 class Router(BaseModel):
@@ -19,9 +59,11 @@ class RouterConnection(BaseModel):
 
 
 class RouterGraph:
+    """ An undirected weighted graph created using an adjacency matrix"""
     def __init__(self):
         self.vertices = {}
         self.matrix = []
+        self.visited = []
 
     def __len__(self):
         return len(self.vertices.keys())
@@ -31,25 +73,29 @@ class RouterGraph:
 
     def add_router(self, name):
         if name not in self.vertices.keys():
-            self.vertices[name] = len(self) + 1  # len(self) + 1 will act as ID for each key
-            self.matrix = [[-1] * len(self) for i in range(len(self))]
+            self.vertices[name] = len(self) + 1  # len(self) + 1 will act as an ID for each key & len -1 will be index
+            self.matrix = [[-1] * len(self) for i in range(len(self))]                    # in the adjacency matrix
             return {"status": "success"}
         else:
             return {"status": "Error, node already exists"}
 
     def add_connection(self, r1, r2, dist=0):
-        if r1 and r2 in self.vertices.keys():
-            r1 = self.vertices[r1] - 1
-            r2 = self.vertices[r2] - 1
-            if self.matrix[r1][r2] <= 0:
-                self.matrix[r1][r2] = dist
-                self.matrix[r2][r1] = dist
-                return {"status": "success"}
+        try:
+            if r1 and r2 in self.vertices.keys():
+                r1 = self.vertices[r1] - 1  # ID value is always 1 more than matrix index
+                r2 = self.vertices[r2] - 1
+                if self.matrix[r1][r2] <= 0:  # set distance in adjacency matrix
+                    self.matrix[r1][r2] = dist
+                    self.matrix[r2][r1] = dist
+                    return {"status": "success"}
+                else:
+                    self.matrix[r1][r2] = dist
+                    self.matrix[r2][r1] = dist
+                    return {"status": "updated"}
             else:
-                self.matrix[r1][r2] = dist
-                self.matrix[r2][r1] = dist
-                return {"status": "updated"}
-        else:
+                return {"status": "Error, router does not exist"}
+
+        except KeyError:
             return {"status": "Error, router does not exist"}
 
     def remove_router(self, name):
@@ -57,11 +103,11 @@ class RouterGraph:
             x = 1
             mx_index = self.vertices[name] - 1
             del self.vertices[name]
-            for key in self.vertices.keys():
+            for key in self.vertices.keys():  # reset and re-order ID in dictionary
                 self.vertices[key] = x
                 x = x + 1
             y = len(self.matrix)
-            for i in range(y):
+            for i in range(y):  # remove row and column of router in question from matrix
                 del self.matrix[i][mx_index]
             del self.matrix[mx_index]
             return {"status": "success"}
@@ -69,7 +115,21 @@ class RouterGraph:
             return
 
     def remove_connection(self, r1, r2):
-        return
+        try:
+            if r1 and r2 in self.vertices.keys():  # reset removed connections in matrix
+                r1 = self.vertices[r1] - 1
+                r2 = self.vertices[r2] - 1
+                self.matrix[r1][r2] = -1
+                self.matrix[r2][r1] = -1
+                return {"status": "success"}
+            elif r1 or r2 not in self.vertices.keys():
+                return
+        except KeyError:
+            return
+
+    def route(self, r1, r2):
+
+        return 0
 
 
 rgraph = RouterGraph()
@@ -80,33 +140,32 @@ def home():
     return {"Data": "Home"}
 
 
-@app.post("/addrouter")
+@app.post("/addrouter", tags=["addrouter"])
 def addrouter(r: Router):
     router = r.name
     return rgraph.add_router(router)
 
 
-@app.post("/connect")
+@app.post("/connect", tags=["connect"])
 def connect(r: RouterConnection):
     r1, r2, dist = r.routerFrom, r.routerTo, int(r.routerDist)
     return rgraph.add_connection(r1, r2, dist)
 
 
-@app.post("/removerouter")
+@app.post("/removerouter", tags=["removerouter"])
 def removerouter(r: Router):
     router = r.name
     return rgraph.remove_router(router)
 
 
-@app.post("/removeconnection")
+@app.post("/removeconnection", tags=["removeconnection"])
 def removeconnection(r: RouterConnection):
     r1, r2 = r.routerFrom, r.routerTo
     return rgraph.remove_connection(r1, r2)
 
-# rgraph.add_router("A")
-# rgraph.add_router("B")
-# rgraph.add_router("C")
-# rgraph.add_router("D")
-# rgraph.add_router("A")
 
-# rgraph.print_matrix()
+@app.post("/route", tags=["route"])
+def route(r: RouterConnection):
+    r1, r2 = r.routerFrom, r.routerTo
+    return rgraph.route(r1, r2)
+
